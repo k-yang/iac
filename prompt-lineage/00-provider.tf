e1d8900a4e29c3dc4743bd1c999ac6334c458b12
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2025-12-04 22:54:15 -0500

    add initial prompt lineage tf code

---
+++ prompt-lineage/00-provider.tf
@@ -0,0 +1,24 @@
+#
+# Provider and Backend configuration
+#
+
+terraform {
+  backend "remote" {
+    organization = "kevinyang"
+
+    workspaces {
+      name = "prompt-lineage"
+    }
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
---
+++ prompt-lineage/01-variables.tf
@@ -0,0 +1,4 @@
+variable "cloudflare_api_token" {
+  type        = string
+  description = "The API token for the Cloudflare API"
+}
